26e830967836ab9bb0c2cee9d2eb27cfaad59e75
Utkarsh Hemrajani <110229028+utkarsh-unravel@users.noreply.github.com>
2026-03-09 20:18:21 +0530

    Update procedures_eu.sql

--- BigQuery_Data_Sharing/Procedures/procedures_eu.sql
+++ BigQuery_Data_Sharing/Procedures/procedures_eu.sql
@@ -13,9 +13,11 @@ CREATE OR REPLACE PROCEDURE unravel_share_EU.export_metadata_EU(
   project_ids ARRAY<STRING>
 )
 BEGIN
-  DECLARE project_id STRING;
+
   DECLARE table_name STRING;
+  DECLARE project_id STRING;
   DECLARE where_clause STRING;
+  DECLARE project_filter STRING;
 
   IF region IS NULL THEN
       RAISE USING MESSAGE = "region is NULL!";
@@ -25,53 +27,132 @@ BEGIN
       RAISE USING MESSAGE = "dataset_name is NULL!";
   END IF;
 
-  IF ARRAY_LENGTH(project_ids) = 0 THEN
-      RAISE USING MESSAGE = "project_ids array is empty!";
-  END IF;
-
   IF ARRAY_LENGTH(tables) = 0 THEN
       RAISE USING MESSAGE = "tables array is empty!";
   END IF;
 
   FOR table_row IN (SELECT * FROM UNNEST(tables)) DO
+
       SET table_name = table_row.f0_;
 
+      -- Create destination table
       BEGIN
           EXECUTE IMMEDIATE FORMAT("""
               CREATE OR REPLACE TABLE `%s.%s_%s` AS
-              SELECT *, "%s" as region, "%s" as project FROM `%s.region-%s.INFORMATION_SCHEMA.%s` WHERE 1 = 0
-          """, dataset_name, table_name, region, region, project_ids[SAFE_OFFSET(0)], project_ids[SAFE_OFFSET(0)], region, table_name);
+              SELECT *, "%s" AS region, "" AS project
+              FROM `region-%s`.INFORMATION_SCHEMA.%s
+              WHERE 1 = 0
+          """,
+          dataset_name,
+          table_name,
+          region,
+          region,
+          region,
+          table_name);
+
       EXCEPTION WHEN ERROR THEN
           RAISE USING MESSAGE = "ERROR: Failed to create " || table_name || " table!";
       END;
 
-      FOR project_row IN (SELECT * FROM UNNEST(project_ids)) DO
-          SET project_id = project_row.f0_;
+      --------------------------------------------------------------------------------
+      -- JOBS and JOBS_TIMELINE → Organization views
+      --------------------------------------------------------------------------------
 
-          IF project_id IS NULL THEN
-              RAISE USING MESSAGE = "ERROR: project_id is NULL!";
+      IF table_name = 'JOBS_BY_ORGANIZATION' THEN
+
+          SET project_filter = CASE
+              WHEN ARRAY_LENGTH(project_ids) > 0
+              THEN "AND project_id IN UNNEST(@project_ids)"
+              ELSE ""
+          END;
+
+          EXECUTE IMMEDIATE FORMAT("""
+              INSERT INTO `%s.%s_%s`
+              SELECT *, "%s" AS region, project_id AS project
+              FROM `region-%s`.INFORMATION_SCHEMA.JOBS_BY_ORGANIZATION
+              WHERE creation_time BETWEEN
+                    TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL %d DAY)
+                    AND CURRENT_TIMESTAMP()
+              %s
+          """,
+          dataset_name,
+          table_name,
+          region,
+          region,
+          region,
+          look_back_days,
+          project_filter)
+          USING project_ids AS project_ids;
+
+      --------------------------------------------------------------------------------
+
+      ELSEIF table_name = 'JOBS_TIMELINE_BY_ORGANIZATION' THEN
+
+          SET project_filter = CASE
+              WHEN ARRAY_LENGTH(project_ids) > 0
+              THEN "AND project_id IN UNNEST(@project_ids)"
+              ELSE ""
+          END;
+
+          EXECUTE IMMEDIATE FORMAT("""
+              INSERT INTO `%s.%s_%s`
+              SELECT *, "%s" AS region, project_id AS project
+              FROM `region-%s`.INFORMATION_SCHEMA.JOBS_TIMELINE_BY_ORGANIZATION
+              WHERE job_creation_time BETWEEN
+                    TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL %d DAY)
+                    AND CURRENT_TIMESTAMP()
+              %s
+          """,
+          dataset_name,
+          table_name,
+          region,
+          region,
+          region,
+          look_back_days,
+          project_filter)
+          USING project_ids AS project_ids;
+
+      --------------------------------------------------------------------------------
+      -- Other tables
+      --------------------------------------------------------------------------------
+
+      ELSE
+
+          IF ARRAY_LENGTH(project_ids) = 0 THEN
+              RAISE USING MESSAGE = "project_ids required for table: " || table_name;
           END IF;
 
-          BEGIN
-              SET where_clause = CASE
-                  WHEN table_name = 'JOBS' THEN
-                      FORMAT("WHERE creation_time BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL %d DAY) AND CURRENT_TIMESTAMP()", look_back_days)
-                  WHEN table_name = 'JOBS_TIMELINE' THEN
-                      FORMAT("WHERE job_creation_time BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL %d DAY) AND CURRENT_TIMESTAMP()", look_back_days)
-                  ELSE
-                      ""
+          FOR project_row IN (SELECT * FROM UNNEST(project_ids)) DO
+
+              SET project_id = project_row.f0_;
+
+              BEGIN
+
+                  EXECUTE IMMEDIATE FORMAT("""
+                      INSERT INTO `%s.%s_%s`
+                      SELECT *, "%s" AS region, "%s" AS project
+                      FROM `%s.region-%s`.INFORMATION_SCHEMA.%s
+                  """,
+                  dataset_name,
+                  table_name,
+                  region,
+                  region,
+                  project_id,
+                  project_id,
+                  region,
+                  table_name);
+
+              EXCEPTION WHEN ERROR THEN
+                  RAISE USING MESSAGE =
+                      "ERROR: Failed to insert into " || table_name ||
+                      " for project: " || project_id;
+
               END;
 
-              EXECUTE IMMEDIATE FORMAT("""
-                  INSERT INTO `%s.%s_%s`
-                  SELECT *, "%s" as region, "%s" as project
-                  FROM `%s`.`region-%s`.INFORMATION_SCHEMA.%s
-                  %s
-              """, dataset_name, table_name, region, region,project_id, project_id, region, table_name, where_clause);
-          EXCEPTION WHEN ERROR THEN
-              RAISE USING MESSAGE = "ERROR: Failed to insert into " || table_name || " table for project: " || project_id;
-          END;
-      END FOR;
-  END FOR;
-END;
+          END FOR;
 
+      END IF;
+
+  END FOR;
+
+END;
